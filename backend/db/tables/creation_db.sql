114b38d7cd4f964b70dcb70e1ff3ee1e40f6a641
Melina Rochon <mroch072@uottawa.ca>
2024-03-04 19:53:08 -0500

    Creation de la base de donnees

---
+++ backend/db/tables/creation_db.sql
@@ -0,0 +1,14 @@
+-- Database: e_hotel_projet
+
+-- DROP DATABASE IF EXISTS e_hotel_projet;
+
+CREATE DATABASE e_hotel_projet
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'English_Canada.1252'
+    LC_CTYPE = 'English_Canada.1252'
+    LOCALE_PROVIDER = 'libc'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1
+    IS_TEMPLATE = False;
