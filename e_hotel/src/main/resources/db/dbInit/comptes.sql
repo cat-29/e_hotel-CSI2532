e49b85f769fa052bae023b742aa804d1e75bbc48
Melina Rochon <mroch072@uottawa.ca>
2024-03-16 11:09:00 -0400

    Creation de comptes pour se connecter au site

---
+++ e_hotel/src/main/resources/db/dbInit/comptes.sql
@@ -0,0 +1,23 @@
+-- COMPTE CLIENT
+CREATE TABLE IF NOT EXISTS compte_client(
+    id_compte SERIAL NOT NULL,
+    id_client CHAR(9),
+    email VARCHAR(255) check (email ~* '^[A-Za-z0-9._+%-]+@[A-Za-z0-9.-]+[.][A-Za-z]+$'),
+    password VARCHAR(255) check (length(password) >= 8),
+    UNIQUE(id_client),
+    UNIQUE(email, password),
+    PRIMARY KEY(id_compte),
+    FOREIGN KEY (id_client) REFERENCES client(nas)
+);
+
+CREATE TABLE IF NOT EXISTS compte_employe(
+    id_compte SERIAL NOT NULL,
+    id_employe CHAR(9),
+    email VARCHAR(255) check (email ~* '^[A-Za-z0-9._+%-]+@[A-Za-z0-9.-]+[.][A-Za-z]+$'),
+    password VARCHAR(255) check (length(password) >= 8),
+    UNIQUE(id_employe),
+    UNIQUE(email, password),
+    PRIMARY KEY(id_compte),
+    FOREIGN KEY (id_employe) REFERENCES employe(nas)
+)
+-- COMPTE EMPLOYE & ADMIN
