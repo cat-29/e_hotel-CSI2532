23df16496536ffcfa0ca0aee3b77bddbf4bc867e
Caterina Bosi <cbosi019@uottawa.ca>
2024-03-29 00:16:43 -0400

    Fixed insert type issues

--- e_hotel/src/main/resources/db/dbInit/insert/insert_employe.sql
+++ e_hotel/src/main/resources/db/dbInit/insert/insert_employe.sql
@@ -1,23 +1,20 @@
 INSERT INTO
     employe (
-        nas, prenom, nom_famille, numero, rue, ville, province, pays, code_postal
+        nas, prenom, nom_famille, numero, rue, ville, province, pays, code_postal, role, id_hotel
     )
 VALUES (
-        123456789, 'Jean', 'Tremblay', 123, 'Rue Sainte-Catherine', 'Montreal', 'QC', 'Canada', 'H2L2M8'
+        '123456789', 'Jean', 'Tremblay', 123, 'Rue Sainte-Catherine', 'Montreal', 'QC', 'Canada', 'H2L2M8', 'EMPLOYE', 1
     ),
     (
-        234567890, 'Marie', 'Gagnon', 456, 'Avenue McGill College', 'Toronto', 'ON', 'Canada', 'M5G2C9'
+        '234567890', 'Marie', 'Gagnon', 456, 'Avenue McGill College', 'Toronto', 'ON', 'Canada', 'M5G2C9', 'EMPLOYE', 1
     ),
     (
-        345678901, 'Pierre', 'Roy', 789, 'Rue Peel', 'Ottawa', 'ON', 'Canada', 'K1P5J2'
+        '345678901', 'Pierre', 'Roy', 789, 'Rue Peel', 'Ottawa', 'ON', 'Canada', 'K1P5J2', 'SUPERVISEUR', 1
     ),
     (
-        456789012, 'Sophie', 'Bouchard', 101, 'Boulevard René-Lévesque', 'Vancouver', 'BC', 'Canada', 'V6Z2W1'
+        '456789012', 'Sophie', 'Bouchard', 101, 'Boulevard René-Lévesque', 'Vancouver', 'BC', 'Canada', 'V6Z2W1', 'SUPERVISEUR', 1
     ),
     (
-        567890123, 'Michel', 'Côté', 202, 'Rue Peel', 'Calgary', 'AB', 'Canada', 'T2P2Y7'
-    ),
-    (
-        678901234, 'Isabelle', 'Morin', 303, 'Rue Sainte-Catherine', 'Sherbrooke', 'QC', 'Canada', 'J1H1Z4'
+        '567890123', 'Michel', 'Côté', 202, 'Rue Peel', 'Calgary', 'AB', 'Canada', 'T2P2Y7', 'GESTIONNAIRE', 1
     )
 ON CONFLICT DO NOTHING;
