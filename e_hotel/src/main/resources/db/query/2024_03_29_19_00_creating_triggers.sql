aa3e8fdb02ef0a5f72ecab4249d9b7cc41f9af24
Melina Rochon <mroch072@uottawa.ca>
2024-03-30 12:44:23 -0400

    Modifier query pour les triggers

--- e_hotel/src/main/resources/db/query/2024_03_29_19_00_creating_triggers.sql
+++ e_hotel/src/main/resources/db/query/2024_03_29_19_00_creating_triggers.sql
@@ -2,7 +2,8 @@
 
 -- Creation des triggers
 
--- FONCTIONS POUR LE TRIGGER 1
+
+-- FONCTION POUR LE TRIGGER 1
 -- Incremente le nombre de chambre dans hotel
 CREATE OR REPLACE FUNCTION add_chambre_hotel() RETURNS 
 TRIGGER 
@@ -18,6 +19,15 @@ BEGIN
 END;
 $$ 
 
+-- TRIGGER 1
+-- Quand une chambre d'hotel est ajoutee, incremente le nombre de chambre
+CREATE TRIGGER add_chambre_hotel
+AFTER INSERT ON chambre FOR EACH ROW
+EXECUTE PROCEDURE add_chambre_hotel ();
+
+
+
+-- FONCTION POUR LE TRIGGER 2
 -- Decremente le nombre de chambre dans la table hotel
 CREATE OR REPLACE FUNCTION remove_chambre_hotel() RETURNS 
 TRIGGER 
@@ -33,37 +43,20 @@ BEGIN
 END;
 $$
 
--- TRIGGER 1
--- Quand une chambre d'hotel est ajoutee, incremente le nombre de chambre
-CREATE TRIGGER add_chambre_hotel
-AFTER INSERT ON chambre FOR EACH ROW
-EXECUTE PROCEDURE add_chambre_hotel ();
-
+-- TRIGGER 2
 -- Quand une chambre d'hotel est supprimee, decremente le nombre de chambre
 CREATE TRIGGER remove_chambre_hotel
 AFTER DELETE ON chambre FOR EACH ROW
 EXECUTE PROCEDURE remove_chambre_hotel ();
 
-INSERT INTO
-    chambre (
-        numero_chambre, id_hotel, prix, capacite_chambre, vue_chambre, capacite_a_etendre
-    )
-VALUES (
-        300, 1, 115.00, 'SIMPLE', 'MER', true
-    )
-ON CONFLICT DO NOTHING;
 
-DELETE FROM chambre WHERE numero_chambre = 300 AND id_hotel = 1;
 
--- TRIGGER 2
-
--- CREATION DES FONCTIONS POUR LE TRIGGER 2
+-- FONCTION POUR LE TRIGGER 3
 CREATE OR REPLACE FUNCTION modifie_nom_chaine_dans_hotel() RETURNS 
 TRIGGER 
 LANGUAGE PLPGSQL AS 
 $$
 BEGIN
-
 	-- Update le nom de la chaine dans la table hotel
 	UPDATE hotel
 	SET
@@ -76,7 +69,6 @@ BEGIN
 	    nom_chaine = NEW.nom_chaine
 	WHERE
 	    nom_chaine = OLD.nom_chaine;
-	-- RETURN NEW;
 
 	-- Update le nom de la chaine dans email_chaine_hoteliere
 	UPDATE email_chaine_hoteliere SET
@@ -87,10 +79,8 @@ BEGIN
 END;
 $$
 
+-- TRIGGER 3
 -- Quand on modifie le nom de la chaine, on la modifie aussi dans hotel
 CREATE TRIGGER modifie_nom_chaine
 AFTER UPDATE ON chaine_hoteliere FOR EACH ROW
 EXECUTE PROCEDURE modifie_nom_chaine_dans_hotel ();
-
-UPDATE chaine_hoteliere SET nom_chaine = 'Hotels Bellevue' where nom_chaine = 'TEST';
-UPDATE chaine_hoteliere SET nom_chaine = 'TEST' where nom_chaine = 'Hotels Bellevue';
