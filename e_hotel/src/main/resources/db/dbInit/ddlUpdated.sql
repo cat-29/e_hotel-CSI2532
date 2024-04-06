ee66ed00fe96ed5a850d310a33d30225c415657b
Caterina Bosi <cbosi019@uottawa.ca>
2024-04-06 16:35:44 -0400

    Fixed duplicate issue

--- e_hotel/src/main/resources/db/dbInit/ddlUpdated.sql
+++ e_hotel/src/main/resources/db/dbInit/ddlUpdated.sql
@@ -390,41 +390,6 @@ $$;
 CREATE TRIGGER add_logs_before_deleting_chambre BEFORE DELETE ON chambre FOR EACH ROW
 EXECUTE PROCEDURE add_logs_before_deleting_chambre ();
 
-CREATE OR REPLACE FUNCTION add_logs_before_deleting_chambre
-() RETURNS TRIGGER 
-LANGUAGE PLPGSQL AS 
-$$
-BEGIN
-	-- Ajoute toutes les lignes dans client_reserve qui doivent etres supprimees
-	INSERT INTO
-	    log_client_reserve (
-	        id_client, numero_chambre, id_hotel, date_checkin, date_checkout, prix, paiement_complete, date_paiement_complete
-	    )
-	SELECT *
-	FROM client_reserve
-	WHERE
-	    numero_chambre = OLD.numero_chambre;
-	-- Ajoute toutes les lignes dans loue_chambre qui doivent etres supprimees
-	INSERT INTO
-	    log_loue_chambre (
-	        numero_chambre, id_hotel, id_client, id_employe, date_checkin, date_checkout, montant_du, paiement_complete, date_paiement_complete
-	    )
-	SELECT *
-	FROM loue_chambre
-	WHERE
-	    numero_chambre = OLD.numero_chambre;
-	-- Supprime les lignes dans client_reserve et loue_chambre
-	DELETE FROM client_reserve
-	where
-	    id_hotel = OLD.id_hotel
-	    and numero_chambre = OLD.numero_chambre;
-	DELETE FROM loue_chambre
-	where
-	    id_hotel = OLD.id_hotel
-	    and numero_chambre = OLD.numero_chambre;
-	RETURN OLD;
-END;
-$$; 
 -- TRIGGER
 
 -- FONCTION POUR LE TRIGGER
