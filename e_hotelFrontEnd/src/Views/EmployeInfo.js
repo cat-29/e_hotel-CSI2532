9230c7ccee7489f14fea6c990bb0587b123dcfc1
Caterina Bosi <cbosi019@uottawa.ca>
2024-04-06 16:01:42 -0400

    Fixed bug where user can delete himself

--- e_hotelFrontEnd/src/Views/EmployeInfo.js
+++ e_hotelFrontEnd/src/Views/EmployeInfo.js
@@ -11,6 +11,8 @@ export const EmployeInfo = () => {
 
   const [modify, setModify] = useState(false);
   const [formDataError, setFormDataError] = useState([]);
+  const [isSelectedEmployeSignedIn, setIsSelectedEmployeSignedIn] =
+    useState(false);
   console.log("ds employe info: ", state);
 
   const [formData, setFormData] = useState({
@@ -45,6 +47,10 @@ export const EmployeInfo = () => {
       roleEmploye: state.employeInfo.roleEmploye,
       idHotel: state.employeInfo.idHotel,
     });
+    if (state.employeInfo.id == state.adminInfo.id) {
+      setIsSelectedEmployeSignedIn(true);
+      console.log("employe is signed in");
+    }
 
     console.log(formData);
   }, []);
@@ -181,25 +187,29 @@ export const EmployeInfo = () => {
               </svg>
               Modifier
             </button>
-            <button
-              type="button"
-              className="btn btn-dark"
-              onClick={() => {
-                handleDeleteSubmit();
-              }}
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="16"
-                height="16"
-                fill="currentColor"
-                className="bi bi-trash3 me-1"
-                viewBox="0 0 15 18"
+            {!isSelectedEmployeSignedIn ? (
+              <button
+                type="button"
+                className="btn btn-dark"
+                onClick={() => {
+                  handleDeleteSubmit();
+                }}
               >
-                <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5M11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47M8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5" />
-              </svg>
-              Supprimer
-            </button>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  className="bi bi-trash3 me-1"
+                  viewBox="0 0 15 18"
+                >
+                  <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5M11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47M8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5" />
+                </svg>
+                Supprimer
+              </button>
+            ) : (
+              <></>
+            )}
           </div>
         ) : (
           <></>
